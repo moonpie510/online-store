33637b352c4abb7e0a84b6b7f1fb6fe82f5bd763
Denis <denstar510@yandex.ru>
2024-10-12 23:43:05 +0300

    Аутентификация. Предварительные тесты

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Установка
+- composer install
 - php artisan shop:install
+- make .env and .env.testing from env.example
 
--- app/Http/Requests/SignUpFormRequest.php
+++ app/Http/Requests/SignUpFormRequest.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Password;
+use Worksome\RequestFactories\Concerns\Pest\FakesRequests;
 
 class SignUpFormRequest extends FormRequest
 {
+    use FakesRequests;
     /**
      * Determine if the user is authorized to make this request.
      */
